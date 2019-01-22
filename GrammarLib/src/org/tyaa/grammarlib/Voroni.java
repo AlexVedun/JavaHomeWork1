c9c4ff6cb137a68125d75dcdbd0706ccb5b18126
Алексей Ефременко <alexeyefr@gmail.com>
2019-01-22 20:44:06 +0200

    HomeWork

--- GrammarLib/src/org/tyaa/grammarlib/Voroni.java
+++ GrammarLib/src/org/tyaa/grammarlib/Voroni.java
@@ -11,8 +11,34 @@ package org.tyaa.grammarlib;
  */
 public class Voroni {
     
-    public static String numToWord(int _number)
+    public static String numToWord(Integer _number)
     {
+        Integer number10 = _number % 10;
+        Integer number100 = _number % 100;
+        
+        if (number100 >= 11 && number100 <= 14) {
+            return "Voron";          
+        } else {
+            switch (number10) {
+
+                case 1: {
+                    return "Vorona";                    
+                }
+                case 2:
+                case 3:
+                case 4: {
+                    return "Voroni";                    
+                }
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 0: {
+                    return "Voron";                    
+                }
+            }
+        }
         return null;
     }
 }
