3aca8789724f0c2c3844d9f97a0a8a98a8a0efe5
Алексей Ефременко <alexeyefr@gmail.com>
2019-01-22 20:45:18 +0200

    HomeWork

--- Voroni/src/org/tyaa/voroni/VoroniUse.java
+++ Voroni/src/org/tyaa/voroni/VoroniUse.java
@@ -20,7 +20,7 @@ public class VoroniUse {
     public static void main(String[] args) {
 
         
-        int number = 0;
+        Integer number = 0;
         Scanner sc = new Scanner(System.in);
 
         MAIN_LOOP : do {
@@ -35,36 +35,8 @@ public class VoroniUse {
             
             //TODO
             //ctrl + space
-            Voroni.numToWord(number);
-                        
-            int number10 = number % 10;
-            int number100 = number % 100;
-            if (number100 >= 11 && number100 <= 14) {
-                System.out.println("Voron");
-            } else {
-                switch (number10) {
-
-                    case 1: {
-                        System.out.println("Vorona");
-                        break;
-                    }
-                    case 2:
-                    case 3:
-                    case 4: {
-                        System.out.println("Voroni");
-                        break;
-                    }
-                    case 5:
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 0: {
-                        System.out.println("Voron");
-                        break;
-                    }
-                }
-            }
+            System.out.println("Answer is: " + number + " " + Voroni.numToWord(number));
+            
             do {
                 System.out.println("Continue? (y/n)");
                 String answer = sc.next();
